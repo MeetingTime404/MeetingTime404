7f9be4c1d33845384416b79e41530059c35ab8af
Muzzh <murdyn@gmail.com>
2017-10-08 17:17:49 +0000

    fixed migrations

--- db/migrate/20171008171602_alter_chess_piece_add_color.rb
+++ db/migrate/20171008171602_alter_chess_piece_add_color.rb
@@ -1,4 +1,4 @@
-class AlterChessPiecesAddUserIdColumn < ActiveRecord::Migration[5.0]
+class AlterChessPieceAddColor < ActiveRecord::Migration[5.0]
   def change
     add_column :chess_pieces, :color, :string
   end
